308e78053b531e55191013c4e6969176d767bb27
Denis1537 <168804196+Denis1537@users.noreply.github.com>
2024-09-18 10:37:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+//номер 1 
+let arr1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+let arr2 = 0
+for (let index = 0; index < arr1.length; index++) {
+    if (arr1[index] % 2 === 0){
+        arr2++
+    }
+}
+console.log(arr2)
+
+//номер 2
+let srednee = 0
+let sum = 0
+for (let index = 0; index < srednee.length; index++) {
+    sum += srednee[index]
+}
+const average = sum / srednee.length
+srednee[4] = average
+console.log(srednee)
+
+//номер 3
+let arr4 = [1,2,3,4,5,6,7,8,9,10,11]
+Math.abs()
+for (let index = 0; index < arr4.length; index++) {
+    Math.abs()
+}
+console.log()
+
+//номер 4
+let massiv = [1,2,3,4,5,6,7,8,9,10]
+let minimalnoe = 0
+let msksimalnoe = arr3[0]
+    for (let index = 1; index < arr3.length; index++) {
+        if(arr[index] > maksimalnoe)
+    }
